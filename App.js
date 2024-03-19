29a61b23815945fa4273cfd6ff8c2805c2415d75
MaxJustino <maxjustino@hotmail.com>
2024-03-19 20:20:25 -0300

    Botões base
    Inserindo botões de cadastro

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StatusBar, Pressable, StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Login no Sistema</Text>
       <StatusBar style="auto" />
-      {/* Adicione os campos de entrada TextInput aqui */}
+      {/* Corrigindo importação e usando TextInput em vez de TextInputBase */}
+      <TextInput />
+      <TextInput />
+
+      {/* Botão de Pressionar para Logar */}
       <Pressable>
         <Text>Logar</Text>
       </Pressable>
+      
+      {/* Botão de Pressionar Esqueci a Senha */}
       <Pressable>
         <Text>Esqueci a senha</Text>
       </Pressable>
+      
       <Pressable>
         <Text>Novo usuário</Text>
       </Pressable>
