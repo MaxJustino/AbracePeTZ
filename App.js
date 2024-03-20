1fc0408ac3a5910b16ec700f3230f9172dbea7aa
MaxJustino <maxjustino@hotmail.com>
2024-03-19 22:07:21 -0300

    Atualização
    Tela de Login

--- App.js
+++ App.js
@@ -1,38 +1,93 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Pressable, StyleSheet, Text, View, TextInput } from 'react-native';
+import { inputStyles } from './styles';
+
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Login no Sistema</Text>
-      <StatusBar style="auto" />
-      {/* Corrigindo importação e usando TextInput em vez de TextInputBase */}
-      <TextInput />
-      <TextInput />
+    <>
+      <View style={styles.container}>
+        <Text style={styles.formTitle}>Login no Sistema</Text>
 
-      {/* Botão de Pressionar para Logar */}
-      <Pressable>
-        <Text>Logar</Text>
-      </Pressable>
-      
-      {/* Botão de Pressionar Esqueci a Senha */}
-      <Pressable>
-        <Text>Esqueci a senha</Text>
-      </Pressable>
-      
-      <Pressable>
-        <Text>Novo usuário</Text>
-      </Pressable>
-    </View>
+        {/* Utilize os estilos importados para os inputs */}
+        <TextInput style={inputStyles.formInput} 
+          placeholder="Informe o seu E-mail"
+          keyboardType='email-address'
+          autoCapitalize="none"
+          autoComplete='email'
+          
+        />
+
+        <TextInput style={inputStyles.formInput}
+          placeholder='Informe a sua Senha'
+          autoCapitalize='none'
+          secureTextEntry
+        />
+
+        <Pressable style={styles.formButton}>
+          <Text style={styles.textButton}>Logar</Text>
+        </Pressable>
+
+        <View style={styles.subContainer}></View>
+
+        <Pressable style={styles.subButton}>
+          <Text style={styles.subTextButton}>Esqueci a senha</Text>
+        </Pressable>
+
+        <Pressable style={styles.subButton}>
+          <Text style={styles.subTextButton}>Novo usuário</Text>
+        </Pressable>
+      </View>
+      <StatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  formTitle: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: 'green',
+    margin: 10,
+  },
+  
+  formButton: {  //Estilos do botão LOGAR
+    
+    backgroundColor: 'green',
+    width: '80%',
+    margin: 10,
+    padding: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  textButton: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  subContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%',
+  },
+  subButton: {
+    padding: 10,
+  },
+  subTextButton: {
+    color: 'blueViolet',
+  },
+
+
+
+  
 });
+
---
+++ styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+
+
+export const inputStyles = StyleSheet.create({
+  formInput: { 
+    color: 'grey',
+    borderColor: 'blueViolet',
+    borderWidth: 1,
+    borderRadius: 10,
+    fontSize: 22,
+    width: '80%',
+    padding: 10,
+    paddingLeft: 20,
+    margin: 10,
+
+  },
+});
+
